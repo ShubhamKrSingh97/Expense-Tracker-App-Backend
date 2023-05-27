9acc23926998783cc541f81485cb1e12211f1341
ShubhamKrSingh97 <shubhkrsingh.1997@gmail.com>
2023-05-27 12:04:13 +0530

    Improved Concurrency

--- controllers/add-expense-controller.js
+++ controllers/add-expense-controller.js
@@ -4,8 +4,7 @@ module.exports = async (req, res) => {
     let { amount, description, category } = req.body;
     let obj = new ExpenseModel(category, amount, description);
     try {
-        let exp = obj.addExpense(req.user.id);
-        let usr=UserModel.findUser(req.user.id);
+        let[exp,usr]=await Promise.all([obj.addExpense(req.user.id),UserModel.findUser(req.user.id)])
         let totalAmt=usr.TotalExpense+Number(amount);
         await UserModel.updateTotalExp(usr.dataValues.id,totalAmt);
         return res.status(202).json({
--- controllers/purchase-premium-controller.js
+++ controllers/purchase-premium-controller.js
@@ -1,10 +1,10 @@
 const Razorpay = require('razorpay');
 const Order = require('../models/order');
 const { User } = require('../models/user');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-function generateToken(id){
-    return jwt.sign({ id: id, premium:true },'secret_key');
+function generateToken(id) {
+    return jwt.sign({ id: id, premium: true }, 'secret_key');
 }
 
 
@@ -31,24 +31,29 @@ const prePayment = async (req, res) => {
 }
 
 const postPayment = async (req, res) => {
+    let { status, payment_id, order_id } = req.body;
+    let userid = req.user.id;
     try {
-        if (req.body.status === "SUCCESS") {
-            await Order.update({
-                status: req.body.status,
-                paymentid: req.body.payment_id,
+        if (status === "SUCCESS") {
+            await Promise.all([Order.update(
+                {
+                    status: status,
+                    paymentid: payment_id,
 
-            }, { where: { orderid: req.body.order_id } });
+                }, { where: { orderid: order_id } }
+            ),
+            User.update(
+                {
+                    PremiumUser: true
+                }, { where: { id: userid } })
 
-
-            await User.update({
-                PremiumUser: true
-            }, { where: { id: req.user.id } });
-            return res.status(202).json({ message: 'You are a Premium User',token:generateToken(req.user.id)});
+            ]);
+            return res.status(202).json({ message: 'You are a Premium User', token: generateToken(userid) });
         }
         else {
             await Order.update({
-                status: req.body.status
-            }, { where: { orderid: req.body.order_id } });
+                status: status
+            }, { where: { orderid: order_id } });
             return res.status(500).json({ message: 'Payment failed' });
         }
     } catch (err) {
